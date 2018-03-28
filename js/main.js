d84861e3e7eebc1bff69c0213f1db49cd693fc4b
nik-el@mail.ru <nik-el@mail.ru>
2018-03-29 00:52:36 +0300

    Вносит правки

--- js/main.js
+++ js/main.js
@@ -1,33 +1,29 @@
 const templates = document.getElementById(`templates`);
-const templateScreensArray = templates.content.querySelectorAll(`section.main`);
+const templateScreens = templates.content.querySelectorAll(`section.main`);
 
 /**
  *  Устанавливаем начальный экран.
  */
-const app = document.querySelector(`.app`);
-const firstAppScreen = document.querySelector(`section.main`);
-let numberOfScreen = 0;
-app.replaceChild(templateScreensArray[numberOfScreen], firstAppScreen);
+const currentScreen = document.querySelector(`.screen`);
+let currentScreenId = 0;
+const renderScreen = (screenId) => {
+  currentScreen.innerHTML = ``;
+  currentScreen.appendChild(templateScreens[screenId]);
+};
+renderScreen(currentScreenId);
 
 /**
  *  Ловим нажатие клавиш [Alt + ← и Alt + →] и переключаем экран.
  */
-let keyControl = {};
-document.addEventListener(`keydown`, (e)=>{
-  const currentAppScreen = document.querySelector(`section.main`);
-  keyControl[e.key] = e.type === `keydown`;
-  if (keyControl.Alt && keyControl.ArrowRight && numberOfScreen < templateScreensArray.length - 1) {
-    numberOfScreen++;
-    app.replaceChild(templateScreensArray[numberOfScreen], currentAppScreen);
-  } else if (keyControl.Alt && keyControl.ArrowLeft && numberOfScreen > 0) {
-    numberOfScreen--;
-    app.replaceChild(templateScreensArray[numberOfScreen], currentAppScreen);
+document.addEventListener(`keydown`, (event)=>{
+  if (!event.altKey) {
+    return;
   }
-});
-document.addEventListener(`keyup`, (e)=>{
-  if (e.key !== `Alt` && keyControl.Alt) {
-    keyControl = {Alt: true};
-  } else {
-    keyControl = {};
+  if (event.key === `ArrowRight` && currentScreenId < templateScreens.length - 1) {
+    currentScreenId++;
+    renderScreen(currentScreenId);
+  } else if (event.key === `ArrowLeft` && currentScreenId > 0) {
+    currentScreenId--;
+    renderScreen(currentScreenId);
   }
 });
