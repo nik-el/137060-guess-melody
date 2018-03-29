380a69a273836018cd00121a2a3c5c078bd036c5
nik-el@mail.ru <nik-el@mail.ru>
2018-03-29 23:29:03 +0300

    Оптимизирует код

--- js/main.js
+++ js/main.js
@@ -1,29 +1,41 @@
 const templates = document.getElementById(`templates`);
 const templateScreens = templates.content.querySelectorAll(`section.main`);
-
-/**
- *  Устанавливаем начальный экран.
- */
 const currentScreen = document.querySelector(`.screen`);
+
+const keys = {
+  arrowLeftKey: 37,
+  arrowRightKey: 39,
+};
 let currentScreenId = 0;
+
+
+const isArrowRightKey = (keyCode) => {
+  return keyCode === keys.arrowRightKey;
+};
+const isArrowLeftKey = (keyCode) => {
+  return keyCode === keys.arrowLeftKey;
+};
+
 const renderScreen = (screenId) => {
   currentScreen.innerHTML = ``;
   currentScreen.appendChild(templateScreens[screenId]);
 };
-renderScreen(currentScreenId);
 
-/**
- *  Ловим нажатие клавиш [Alt + ← и Alt + →] и переключаем экран.
- */
-document.addEventListener(`keydown`, (event)=>{
-  if (!event.altKey) {
-    return;
-  }
-  if (event.key === `ArrowRight` && currentScreenId < templateScreens.length - 1) {
-    currentScreenId++;
-    renderScreen(currentScreenId);
-  } else if (event.key === `ArrowLeft` && currentScreenId > 0) {
-    currentScreenId--;
-    renderScreen(currentScreenId);
-  }
-});
+const switchScreen = () => {
+  document.addEventListener(`keydown`, (event) => {
+    if (!event.altKey) {
+      return;
+    }
+    if (isArrowRightKey(event.keyCode) && currentScreenId < templateScreens.length - 1) {
+      currentScreenId++;
+      renderScreen(currentScreenId);
+    } else if (isArrowLeftKey(event.keyCode) && currentScreenId > 0) {
+      currentScreenId--;
+      renderScreen(currentScreenId);
+    }
+  });
+};
+
+renderScreen(currentScreenId);
+switchScreen();
+
