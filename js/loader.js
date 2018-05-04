dc9153ece37f069e623ed4f15d83cb5649dcf39a
Nikita Litvinenko <nik-el@mail.ru>
2018-05-04 22:38:25 +0300

    add draft preload

--- js/loader.js
+++ js/loader.js
@@ -1,8 +1,8 @@
 import {adaptData} from './game/game-adapter';
 
 const SERVER_URL = `https://es.dump.academy/guess-melody`;
-
 const APP_ID = 7777777;
+
 
 const checkStatus = (response) => {
   if (response.ok) {
@@ -12,11 +12,40 @@ const checkStatus = (response) => {
   }
 };
 
+const getAudioUrls = (questions) => {
+  const audioUrls = [];
+
+  for (const question of questions) {
+    if (question.type === `artist`) {
+      audioUrls.push(question.src);
+    } else if (question.type === `genre`) {
+      question.answers.forEach((answer) => {
+        audioUrls.push(answer.src);
+      });
+    }
+  }
+  return audioUrls;
+};
+
+const loadAudio = (url) => {
+  return new Promise((resolve, reject) => {
+    const audio = new Audio();
+    audio.oncanplay = () => resolve(audio);
+    audio.onerror = () => reject(`Не удалось загрузить аудио: ${url}`);
+    audio.src = url;
+  });
+};
+
 const toJSON = (res) => res.json();
 
 export default class Loader {
   static loadData() {
-    return fetch(`${SERVER_URL}/questions`).then(checkStatus).then(toJSON).then(adaptData);
+    return fetch(`${SERVER_URL}/questions`)
+      .then(checkStatus)
+      .then(toJSON)
+      .then(adaptData)
+      .then((questions) => getAudioUrls(questions).map((audio) => loadAudio(audio)))
+      .then((audioPromises) => Promise.all(audioPromises));
   }
 
   static loadResults() {
